7e24858234ebb4562f0b0fad94fbf46c04461efe
Pedro Duarte <phduarte2704@gmail.com>
2024-03-10 16:04:17 -0300

    adição dos efeitos de hover, estilização da terceira seção e termino do projeto

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const botaoabrir = document.querySelector("header > button")
-const botaofechar = document.querySelector("header nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
 const nav = document.querySelector("header .mobile")
 
-botaoabrir.addEventListener('click', abrirmenu)
-botaofechar.addEventListener('click', fecharmenu)
+botaoAbrir.addEventListener('click', abrirMenu)
+botaoFechar.addEventListener('click', fecharMenu)
 
-function abrirmenu() {
+function abrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
 }
 
-function fecharmenu() {
+function fecharMenu() {
     body.classList.remove("escurecer")
     nav.classList.remove("abrir")
 }
